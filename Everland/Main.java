3ddd18ea18ce5ad1a6c3b0b43bb0c47b892d7a80
DESKTOP-BP71QIA\정해석 <tiya516@naver.com>
2023-04-26 13:44:09 +0900

    놀이동산 v1 (권종, 주민번호, 티켓수, 우대사항) : 나이계산

--- Everland/Main.java
+++ Everland/Main.java
@@ -5,13 +5,13 @@ public class Main{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		InputClass inputClass = new InputClass();
-//		
-//		inputClass.Select_ticket();
+		
+		inputClass.Select_ticket();
 		String age = inputClass.Select_id();
-//		inputClass.Select_ticketNum();
-//		inputClass.Select_Udea();
-//		
-//		
+		inputClass.Select_ticketNum();
+		inputClass.Select_Udea();
+		
+		
 		calculateClass cc = new calculateClass();
 		System.out.println(cc.getAge(age)); // 나이확인
 		

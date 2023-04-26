3405664d76873f62122e6345c8885cfea8462d01
DESKTOP-BP71QIA\정해석 <tiya516@naver.com>
2023-04-26 15:45:18 +0900

    놀이동산 매표 v2 필요한 클래스 추가 ( 최종 표 정보 표시, 에러 표시)

---
+++ Everland/finalPrint.java
@@ -0,0 +1,18 @@
+package Everland;
+
+public class finalPrint {
+	public String[] print_Finalticket(int[] ticketList) {
+		String[] info = new String[6];
+		
+		// 권종
+		if (ticketList[0] == 1) {
+			info[0] = "주간권";
+		} else {
+			info[1] = "야간권";
+		}
+		
+		// 나이
+		
+		return info;
+	}
+}
---
+++ Everland/printError.java
@@ -0,0 +1,8 @@
+package Everland;
+
+public class printError {
+	public void printerror() {
+		System.out.printf("잘 못 입력하셨습니다.");
+	}
+
+}
